72d00456f698b30886dca02946df7b86bc14dcb7
Raushan Kumar <raushan376kumar@gmail.com>
2023-06-16 01:55:14 +0530

    Examify Backend Project Configured

---
+++ ExamifyServer/src/main/java/platform/examify/ExamifyApplication.java
@@ -0,0 +1,13 @@
+package platform.examify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamifyApplication.class, args);
+	}
+
+}

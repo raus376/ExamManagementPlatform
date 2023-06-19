cce1b95b30e0d8ceef39e20ba8deb678b8fb229e
Raushan Kumar <raushan376kumar@gmail.com>
2023-06-19 22:55:25 +0530

    User Register Service Updated

--- ExamifyClient/src/app/app.module.ts
+++ ExamifyClient/src/app/app.module.ts
@@ -4,14 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
+import { MatButtonModule } from '@angular/material/button';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { LoginComponent } from './pages/login/login.component';
-import {MatInputModule} from '@angular/material/input';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {FormsModule} from '@angular/forms'
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
 
 @NgModule({
   declarations: [
@@ -20,6 +23,7 @@ import {FormsModule} from '@angular/forms'
     FooterComponent,
     SignupComponent,
     LoginComponent,
+
   ],
   imports: [
     BrowserModule,
@@ -29,6 +33,8 @@ import {FormsModule} from '@angular/forms'
     MatInputModule,
     MatFormFieldModule,
     FormsModule,
+    HttpClientModule,
+    MatSnackBarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ExamifyClient/src/app/pages/signup/signup.component.ts
+++ ExamifyClient/src/app/pages/signup/signup.component.ts
@@ -1,26 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
-constructor() {}
+export class SignupComponent  implements OnInit {
+  constructor(private userService: UserService) { }
 
-public User={
-  userName:'',
-  password:'',
-  firstName:'',
-  lastName:'',
-  email:'',
-  mobile:'',
-}
+  public User = {
+    userName: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+    email: '',
+    mobile: '',
+    profile_image:''
+  }
 
-ngOnInit():void {}
+  ngOnInit(): void { }
 
-formSubmit(){
-  alert("submit")
-  console.log(this.User)
-}
+  formSubmit() {
+
+    if (this.User.userName == '' || this.User.userName == null) {
+      Swal.fire("username required")
+      return;
+    }
+
+    //create User function call
+    this.userService.addUser(this.User).subscribe(
+      (data:any) => {
+        console.log(data);
+        
+        Swal.fire(
+          'success',
+          'Register Successfully',
+          'success'
+        )
+      },
+      (error) => {
+        console.log(error);
+        Swal.fire("Some thing went wrong")
+      }
+    )
+  }
 }

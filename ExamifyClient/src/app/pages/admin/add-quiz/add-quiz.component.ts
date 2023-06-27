dfafd9b5143bfaaf147966f73fa28a9f86bf61c2
Raushan Kumar <raushan376kumar@gmail.com>
2023-06-27 15:36:04 +0530

    Add Quiz Data Binding Completed

--- ExamifyClient/src/app/pages/admin/add-quiz/add-quiz.component.ts
+++ ExamifyClient/src/app/pages/admin/add-quiz/add-quiz.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryService } from 'src/app/services/category.service';
+import { QuizService } from 'src/app/services/quiz.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-add-quiz',
@@ -7,23 +10,110 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddQuizComponent implements OnInit{
   
-  categories=[
-    {
-      cid:32,
-      title:'title'
-    },
-    {
-      cid:32,
-      title:'title'
-    },
-    {
-      cid:32,
-      title:'title'
+  constructor(private _category:CategoryService,private _quiz:QuizService){};
+
+  categories:any=[];
+  
+  quizData={
+    title:'',
+    description:'',
+    maxMark:'',
+    numberOfQuestions:'',
+    isActive:true,
+    category:{
+      cid:''
     }
-  ];
+  }
   
   
+  ngOnInit(): void {
+    this._category.category().subscribe((data:any)=>{
+      this.categories=data;
+    },
+    (error)=>{
+      Swal.fire("Server Error !");
+      console.log(error);
+    })
+  }
+
+  public addQuiz(){
+    
+    if(this.quizData.title.trim()=='' || this.quizData.title==null){
+      Swal.fire({
+        title: 'Title Required',
+        icon: 'error',
+        showConfirmButton: true,
+        timer: 2500 // 4 seconds
+      })
+    }
+
+    if(this.quizData.description.trim()=='' || this.quizData.description==null){
+      Swal.fire({
+        title: 'Description Required',
+        icon: 'error',
+        showConfirmButton: true,
+        timer: 2500 // 4 seconds
+      })
+  }
+
+  if(this.quizData.maxMark=='' || this.quizData.maxMark==null){
+    Swal.fire({
+      title: 'Maximum_Marks Required',
+      icon: 'error',
+      showConfirmButton: true,
+      timer: 2500 // 4 seconds
+    })
+}
+
+if(this.quizData.numberOfQuestions=='' || this.quizData.numberOfQuestions==null){
+  Swal.fire({
+    title: 'Number_Of_Questions Required',
+    icon: 'error',
+    showConfirmButton: true,
+    timer: 2500 // 4 seconds
+  })
+}
+
+if(this.quizData.category.cid=='' || this.quizData.category.cid==null){
+  Swal.fire({
+    title: 'Category Required !',
+    icon: 'error',
+    showConfirmButton: true,
+    timer: 2500 // 4 seconds
+  })
+}
+
+
+ this._quiz.addQuiz(this.quizData).subscribe((data)=>{
   
-  ngOnInit(): void {}
+  // this.quizData={
+  //   title:'',
+  //   description:'',
+  //   maxMark:'',
+  //   numberOfQuestions:'',
+  //   isActive:true,
+  //   category:{
+  //     cid:''
+  //   }
+  // }
+  
+  Swal.fire({
+    title: 'Quiz Added Successfully',
+    icon: 'success',
+    showConfirmButton: true,
+    timer: 2500 // 4 seconds
+  })
+ },
+ (error)=>{
+  console.log(error);
+  Swal.fire({
+    title: 'Server Error !',
+    icon: 'error',
+    showConfirmButton: true,
+    timer: 2500 // 4 seconds
+  })
+ })
+
+  }
 
 }
--- ExamifyClient/src/app/services/quiz.service.ts
+++ ExamifyClient/src/app/services/quiz.service.ts
@@ -9,7 +9,13 @@ export class QuizService {
 
   constructor(private _http:HttpClient) { }
 
+  // get All Quize
   public quizzes(){
     return this._http.get(`${baseUrl}/quiz/getAll`)
+  }
+
+  //create Quiz
+  public addQuiz(quiz:any){
+    return this._http.post(`${baseUrl}/quiz/create`,quiz);
   }
 }
